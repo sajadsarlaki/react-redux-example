dfdd88acbf2396cb00579693349ccfbb1a345406
sajad <sajadsarlaki2017@gmail.com>
2022-05-28 17:58:27 +0430

    Basic redux, for a counter implemented.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+// ---------------------------- redux ------------------------------------
+import { createStore } from 'redux';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// *********************************************************************************************
+/*//-------------------------------------------- basic redux --------------------------------
+//Store -> global state
+//Action: type of things we can do for example: increment the counter
+const increment = () =>{
+  return{
+    type: 'INCREMENT'
+  }
+}
+const decrement = () =>{
+  return{
+    type: 'DECREMENT'
+  }
+} 
+// --------------------------------------------------
+//Reducer: The responses to the actions
+const counterReducer = (state =0, action) => {
+    switch(action.type){
+      case 'INCREMENT':
+        return state + 1;
+      case 'DECREMENT':
+        return state - 1;  
+}
+}
+let store = createStore(counterReducer);
+store.subscribe(()=>console.log(store.getState()));
+// --------------------------------------------------
+//Dispatch: trigger the reducer and execute the action
+
+store.dispatch(increment())
+store.dispatch(increment())
+store.dispatch(increment())
+//***************************************************************************************** 
+*/
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
